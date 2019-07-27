bc041f238dc9dc29f12b54040e82c11f00d3c5a5
Felix Zhao <felixz2010@gmail.com>
2019-07-27 12:09:22 +1200

    add request field required.

--- ContentExtractionService/Models/Request.cs
+++ ContentExtractionService/Models/Request.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ContentExtractionService.Models
 {
     public class Request
     {
+        [Required]
         public String EmailContent { get; set; }
     }
 }
