e7421a0fdad4fa0b0fe8f03bd246523e0e9ea080
Felix Zhao <felixz2010@gmail.com>
2019-07-29 11:15:12 +1200

    add more controller unit test.

--- ControllerTests/ContentsControllerTests.cs
+++ ControllerTests/ContentsControllerTests.cs
@@ -37,7 +37,77 @@ namespace ControllerTests
             Assert.NotNull(result);
             var okObjectResult = Assert.IsType<OkObjectResult>(result);
             var response = Assert.IsType<Response>(okObjectResult.Value);
-            Assert.Equal(response.Expense.Total, 123.12M);
+            Assert.Equal( 123.12M, response.Expense.Total);
+        }
+
+        [Fact]
+        public void NoTotalTest()
+        {
+            var mapper = new Mock<IMapper>();
+            mapper.Setup(f => f.Map<Response>(It.IsAny<RelevantDataBO>()))
+                .Returns(new Response() { });
+
+            var extractor = new Mock<IContentExtractor>();
+            extractor.Setup(f => f.GetRelevantData(It.IsAny<string>(), out It.Ref<RelevantDataBO>.IsAny))
+                .Returns(false);
+
+            var controller = new ContentsController(mapper.Object, extractor.Object);
+
+            var request = new Request()
+            {
+                EmailContent = "abc"
+            };
+
+            var result = controller.Extract(request);
+
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void ExceptionTest1()
+        {
+            var mapper = new Mock<IMapper>();
+            mapper.Setup(f => f.Map<Response>(It.IsAny<RelevantDataBO>()))
+                .Returns(new Response() { });
+
+            var extractor = new Mock<IContentExtractor>();
+            extractor.Setup(f => f.GetRelevantData(It.IsAny<string>(), out It.Ref<RelevantDataBO>.IsAny))
+                .Throws(new Exception());
+
+            var controller = new ContentsController(mapper.Object, extractor.Object);
+
+            var request = new Request();
+           
+            var result = controller.Extract(request);
+
+            Assert.NotNull(result);
+            var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
+            Assert.NotNull(statusCodeResult);
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public void ExceptionTest2()
+        {
+            var mapper = new Mock<IMapper>();
+            mapper.Setup(f => f.Map<Response>(It.IsAny<RelevantDataBO>()))
+                .Throws(new Exception());
+
+            var extractor = new Mock<IContentExtractor>();
+            extractor.Setup(f => f.GetRelevantData(It.IsAny<string>(), out It.Ref<RelevantDataBO>.IsAny))
+                .Returns(true);
+
+            var controller = new ContentsController(mapper.Object, extractor.Object);
+
+            var request = new Request();
+
+            var result = controller.Extract(request);
+
+            Assert.NotNull(result);
+            var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
+            Assert.NotNull(statusCodeResult);
+            Assert.Equal(500, statusCodeResult.StatusCode);
         }
     }
 }
